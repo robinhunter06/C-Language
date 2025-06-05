08949acea74e9ad95c7dea5455c8b202712d57bc
Dipak Shelkande <dipakshelkand12@gmail.com>
2025-06-05 12:44:06 +0530

    chapeter 1 complete

---
+++ chapter 1/areaofcircle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+// Area of Circle
+
+int main(){
+    float radius;
+    scanf("%f",&radius);
+    printf("%f",3.14 * radius * radius);
+    return 0;
+}
---
+++ chapter 1/areaofsquare.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+// Area of a Square
+int main(){
+    int side;
+    scanf("%d",&side);
+    printf("%d",side*side);
+    return 0;
+}
